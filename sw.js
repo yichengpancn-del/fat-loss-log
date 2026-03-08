74387b66607a32a9facad99e0bdc8a0a69dd18ea
yichengpancn-del <yichengpan.cn@gmail.com>
2026-03-08 12:54:20 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,49 @@
+const CACHE_NAME = "fat-loss-log-v2";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./manifest.webmanifest"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys.map((key) => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+          return Promise.resolve();
+        })
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    caches.match(event.request).then((cached) => {
+      if (cached) return cached;
+
+      return fetch(event.request)
+        .then((response) => {
+          const cloned = response.clone();
+          caches.open(CACHE_NAME).then((cache) => {
+            cache.put(event.request, cloned);
+          });
+          return response;
+        })
+        .catch(() => caches.match("./index.html"));
+    })
+  );
+});
